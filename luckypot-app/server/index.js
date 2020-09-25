6e0eab6eea3266e0160f44bafe74da97af260628
Michele <barbmich.since86@gmail.com>
2020-09-25 17:03:38 -0400

    add hidden files like .gitignore that were left behind lol

---
+++ luckypot-app/server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const pino = require("express-pino-logger")();
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(pino);
+
+app.get("/api/greeting", (req, res) => {
+  const name = req.query.name || "World";
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+});
+
+app.get("/api/testing", (req, res) => {
+  const name = req.query.name || "World";
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify({ greeting: `WTF ${name}!` }));
+});
+
+app.listen(3001, () =>
+  console.log("Express server is running on localhost:3001")
+);
