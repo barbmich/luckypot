4b5c34b83ee41e99566ef8f00c24d9b59f8f7b71
Devin Coughlin <devin_coughlin@hotmail.com>
2020-09-25 21:13:11 -0400

    initial Nav bar setup with dead buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 export default function App(props) {
   const [state, setState] = useState({
@@ -30,32 +31,6 @@ export default function App(props) {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="name">Enter your name: </label>
-          <input
-            id="name"
-            type="text"
-            value={state.name}
-            onChange={handleChange}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{state.greeting}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NavBar />
   );
 }
