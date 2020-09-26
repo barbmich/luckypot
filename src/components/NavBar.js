fc38e033b34f1f25d02f9431fb7b07d00b76ad40
Devin Coughlin <devin_coughlin@hotmail.com>
2020-09-25 22:09:23 -0400

    CSS nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -22,7 +22,7 @@ export default function NavBar(props) {
         <ul class="nav navbar-nav navbar-right">
             <li><a href="/"><span class="glyphicon glyphicon-log-in"></span>     Login</a></li>
             <li><a href="/"><span class="glyphicon glyphicon-user"></span>     Sign Up</a></li>
-            <li><a href="/logout"><span class="glyphicon glyphicon-log-out"></span>     Logout</a></li>
+            <li><a href="/"><span class="glyphicon glyphicon-log-out"></span>     Logout</a></li>
         </ul>
       </div>
     </nav>
