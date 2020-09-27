fad667cb0c40fe50764f3e3865260cbdc199fd61
Daniel Nascimento <danielhn@gmail.com>
2020-09-27 17:28:00 -0400

    Route to add custom recipe

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,14 +8,13 @@ module.exports = (db) => {
     for (key in req.body) {
       values.push(req.body[key])
     };
-    // Promise.all([])
     db.query(`SELECT id, first_name, last_name, email, avatar_url 
               FROM users
               WHERE email = $1 AND password = $2;
               `, values)
       .then(data => {
         user = data.rows[0];
-        res.send(user);
+        res.json({ user });
       })
       .catch(err => {
         res
@@ -29,38 +28,14 @@ module.exports = (db) => {
     for (key in req.body) {
       values.push(req.body[key])
     };
-    console.log(values)
-    db.query(
-      `
-      INSERT INTO users (first_name, last_name, email, password)
-      VALUES ($1, $2, $3, $4) RETURNING *;
-      `, values)
+    console.log(values);
+    db.query(`INSERT INTO users (first_name, last_name, email, password)
+              VALUES ($1, $2, $3, $4) 
+              RETURNING *;
+              `, values)
       .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .send('not working')
-          // .json({ error: err.message });
-        });
-  });
-
-  router.get("/signup", (req, res) => {
-    const values = [];
-    for (key in req.body) {
-      values.push(req.body[key])
-    };
-    console.log(values)
-    db.query(
-      `
-      INSERT INTO users (first_name, last_name, email, password)
-      VALUES ($1, $2, $3, $4) RETURNING *;
-      `, values)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
+        const user = data.rows[0];
+        res.json({ user });
       })
       .catch(err => {
         res
