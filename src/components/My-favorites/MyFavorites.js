a12c9266b619d543d8f94f6b5211c3e7573921bf
Michele <barbmich.since86@gmail.com>
2020-09-29 13:18:47 -0400

    remove original app folders

--- src/components/My-favorites/MyFavorites.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function MyFavorites(props) {
-  return (
-    <div className="myFavorites">
-      <h1 className="pageTitle">Favorite Recipes</h1>
-    </div>
-  );
-}
