59f0b0c3e60fc59c22d9d0214eb94dc8027b02cc
Daniel Nascimento <danielhn@gmail.com>
2020-09-29 13:15:07 -0400

    Change folders etc

--- src/components/My-potlucks/MyPotlucks.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function MyPotlucks(props) {
-  return (
-    <div className="myPotlucks">
-      <h1 className="pageTitle">My Potlucks</h1>
-    </div>
-  );
-}
