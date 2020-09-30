0cd2ce6ec4b572ca6a033429deea37c4fe49c056
Michele <barbmich.since86@gmail.com>
2020-09-30 00:26:08 -0400

    changed users route to auth to better match functionality, add query to get potlucks from user

--- server/index.js
+++ server/index.js
@@ -12,9 +12,10 @@ const { Pool } = require("pg");
 const dbParams = require("./lib/db.js");
 const db = new Pool(dbParams);
 
-const usersRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
 const recipesRoutes = require("./routes/recipes");
-const favorites = require("./routes/favorites");
+const favoritesRoutes = require("./routes/favorites");
+const potluckRoutes = require("./routes/potlucks");
 db.connect();
 
 app.use(cors());
@@ -22,9 +23,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(pino);
 
-app.use("", usersRoutes(db));
+app.use("", potluckRoutes(db));
+app.use("", authRoutes(db));
 app.use("", recipesRoutes(db));
-app.use("", favorites(db));
+app.use("", favoritesRoutes(db));
 
 app.listen(PORT, () =>
   console.log(`Express server is running on port ${PORT}`)
--- server/routes/potlucks.js
+++ server/routes/potlucks.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
-// add receipe to favorites list
+// gets potlucks list
 module.exports = (db) => {
+  router.get("/mypotlucks/:userId", (req, res) => {
+    const values = [req.params.userId];
+    console.log(values);
+    db.query(
+      `
+      SELECT * FROM events JOIN users ON events.owner_id = users.id WHERE users.id = $1;
+      `,
+      values
+    )
+      .then((data) => {
+        console.log(data.rows);
+        res.json(data.rows);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send(err);
+      });
+    // })
+  });
+
   router.post("/favorites/add", (req, res) => {
     const values = [];
     for (key in req.body) {
