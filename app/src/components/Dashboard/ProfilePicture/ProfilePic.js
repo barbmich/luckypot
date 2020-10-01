bcd782b585d167fe30601065df16dae0606b7ea7
Daniel Nascimento <danielhn@gmail.com>
2020-10-01 16:49:29 -0400

    Start changing profile picture

--- app/src/components/Dashboard/ProfilePicture/ProfilePic.js
+++ app/src/components/Dashboard/ProfilePicture/ProfilePic.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./ProfilePic.scss";
 import { Figure } from "react-bootstrap";
+const classNames = require('classnames');
 
 export default function ProfilePic(props) {
   const { avatar_url, first_name, last_name } = props;
+
+  const picClass = classNames('pic',
+    { 'pic':1,
+      'pic--no': 0,
+      'pic--yes': 2 
+    }
+  );
+
   return (
-    <Figure className="pic">
+    <Figure className={picClass}>
       <Figure.Image
         width={171}
         height={180}
