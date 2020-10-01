8a7f1bcb703564fadfb612e73f11e006e5eda8a8
Daniel Nascimento <danielhn@gmail.com>
2020-09-30 23:59:41 -0400

    Add route to users list for dashboard

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
-  router.get("/dashboard/:userId/", (req, res) => {
+  router.get("/dashboard/events/:userId/", (req, res) => {
     const values = [parseInt(req.params.userId, 10)];
-    console.log(typeof values[0]);
-      db.query(`SELECT DISTINCT events.id AS id,
+    
+      db.query(`
+      SELECT DISTINCT events.id AS id,
       events.owner_id AS owner_id,
       events.name AS event_name, 
       events.date AS date, 
@@ -22,15 +23,47 @@ module.exports = (db) => {
       WHERE user_id = $1);
       `, values)
       .then((data) => {
-        console.log(typeof values[0]);
         console.log(data.rows);
-        res.json(data.rows);
+        let events = data.rows;
+        res.json(events);
       })
       .catch((err) => {
         console.log(err);
         res.send(err);
       });
   });
+
+  router.get("/dashboard/users/:userId/", (req, res) => {
+    const values = [parseInt(req.params.userId, 10)];
+    db.query(`
+    SELECT events.id AS event_id,
+       users.id AS id,
+       users.first_name AS first_name, 
+       users.last_name AS first_name,events.date, 
+       users.email AS email, 
+       users.avatar_url AS avatar_url
+       FROM guest_details
+       JOIN events ON events.id = guest_details.event_id
+       JOIN items ON events.id = items.event_id
+       JOIN users ON users.id = guest_details.user_id
+       WHERE events.id IN(SELECT event_id
+       FROM guest_details
+       WHERE user_id = $1);
+    `, values)
+    .then((data) => {
+      console.log(data.rows);
+      let users = data.rows;
+      res.json(users);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(err);
+    });
+
+
+  })
+    
+
 
   return router;
 };
