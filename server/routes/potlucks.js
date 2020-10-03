2734fe6281ad7593fb2bf8f8380ed822db08e68a
Daniel Nascimento <danielhn@gmail.com>
2020-10-03 15:53:49 -0400

    Create potluck and tinyurl

--- server/routes/potlucks.js
+++ server/routes/potlucks.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const prettylink = require('prettylink');
 
 // gets potlucks list
 module.exports = (db) => {
@@ -15,7 +16,8 @@ module.exports = (db) => {
       events.address AS address,
       events.post_code AS post_code,
       events.city AS city,
-      events.province AS province
+      events.province AS province,
+      events.tiny_url AS tiny_url
       FROM guest_details
       JOIN events ON events.id = guest_details.event_id
       JOIN items ON events.id = items.event_id
@@ -49,16 +51,40 @@ module.exports = (db) => {
     db.query(
       `
       INSERT INTO events (owner_id, name, date, address, post_code, city, province)
-      VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;
+      VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id;
       `,
       values
     )
-      .then((data) => {
-        res.json(data.rows);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    .then((data) => {
+        const event_id = [data.rows[0].id];
+        const tinyurl = new prettylink.TinyURL();
+        const tiny = tinyurl.short(`http://localhost:3002/dashboard/${event_id}`);
+        return Promise.all([tiny, event_id]);
+        })
+        .then(([tiny, event_id]) => {
+            const values = [tiny, event_id[0]];
+            console.log(values);
+            return db.query(
+              `
+              UPDATE events SET tiny_url=$1 WHERE id=$2 RETURNING id, owner_id;
+              `
+            , values)
+          })
+            .then((data) => {
+              const values = [data.rows[0].id, data.rows[0].owner_id]
+              return db.query(
+                `
+                INSERT INTO guest_details (event_id, user_id) 
+                VALUES ($1, $2) RETURNING *;
+                `
+              , values)
+            })
+              .then((data) => {
+                res.send(data.rows)
+              }) 
+              .catch((err) => {
+              console.log(err);
+            });
   });
 
   // router.post("/create", (req, res) => {
