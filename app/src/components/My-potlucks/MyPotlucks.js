1951a7c3f230e3028c5e28a91d1d27f09e5c2c96
Michele <barbmich.since86@gmail.com>
2020-10-05 18:20:35 -0400

    improve mypotlucks page look

--- app/src/components/My-potlucks/MyPotlucks.js
+++ app/src/components/My-potlucks/MyPotlucks.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import moment from "moment";
+import { Card, Button } from "react-bootstrap";
+import "./MyPotlucks.scss";
 
 export default function MyPotlucks(props) {
   const { loggedUser } = props;
+  let history = useHistory();
 
   const [isLoading, setLoading] = useState(true);
   const [potlucksList, setPotlucksList] = useState([]);
@@ -13,11 +16,14 @@ export default function MyPotlucks(props) {
     axios
       .get(`http://localhost:3003/mypotlucks/${loggedUser.id}`)
       .then((result) => {
-        console.log("this is result:", result.data);
         setPotlucksList(result.data);
         setLoading(false);
       });
   }, []);
+
+  function visitPotluck(potluck) {
+    history.push(`/dashboard/${potluck.unique_key}`);
+  }
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -25,15 +31,38 @@ export default function MyPotlucks(props) {
 
   const userPotlucks = potlucksList.map((potluck, i) => {
     if (potluck) {
+      console.log(potluck);
       return (
-        <div key={i}>
-          <h3>{potluck.event_name}</h3>
-          <p>{moment(potluck.date).format("dddd, MMMM Do YYYY, h:mm a")}</p>
-          <p>
-            <Link to={`/dashboard/${potluck.unique_key}`}>Get details</Link>
-          </p>
-          <a href={potluck.tiny_url}>Invite your friends!</a>
-        </div>
+        <Card
+          key={i}
+          className="text-center potluck-item"
+          style={{ border: "solid #282c34 3px" }}
+        >
+          <Card.Body>
+            <Card.Header>
+              {" "}
+              {`${moment(potluck.date).format("dddd, MMMM Do YYYY, h:mm a")}`}
+            </Card.Header>
+            <Card.Title>{potluck.event_name}</Card.Title>
+            <Card.Text>{`${potluck.address}, ${potluck.city} ${potluck.post_code} - ${potluck.province}`}</Card.Text>
+            <Card.Text></Card.Text>
+            <Button onClick={() => visitPotluck(potluck)} variant="primary">
+              Visit the potluck dashboard{" "}
+            </Button>
+          </Card.Body>
+          <Card.Footer className="text-muted">
+            <span>{moment(potluck.date).fromNow()}</span>
+          </Card.Footer>
+        </Card>
+
+        // <div key={i}>
+        //   <h3>{potluck.event_name}</h3>
+        //   <p>{moment(potluck.date).format("dddd, MMMM Do YYYY, h:mm a")}</p>
+        //   <p>
+        //     <Link to={`/dashboard/${potluck.unique_key}`}>Get details</Link>
+        //   </p>
+        //   <a href={potluck.tiny_url}>Invite your friends!</a>
+        // </div>
       );
     } else {
       return (
