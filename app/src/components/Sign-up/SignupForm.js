a3cd8903ce1ef7d0a033b9553842c3060968c4e1
Michele <barbmich.since86@gmail.com>
2020-10-05 17:12:25 -0400

    final signup look

--- app/src/components/Sign-up/SignupForm.js
+++ app/src/components/Sign-up/SignupForm.js
@@ -9,7 +9,6 @@ import FileBase64 from "react-file-base64";
 export default function SignupForm(props) {
   const location = useLocation();
   const urlGiven = location.state ? location.state.urlGiven : null;
-  const directUrl = location.state ? location.state.directUrl : null;
   const { setAuth, setLoggedUser } = props;
   const [error, setError] = useState("");
   const [firstName, setFirstName] = useState("");
@@ -21,19 +20,17 @@ export default function SignupForm(props) {
 
   let history = useHistory();
 
-  console.log("urlGiven FROM Signin::", directUrl);
+  console.log("urlGiven FROM Signin::", urlGiven);
 
   function getFile(image) {
     setAvatar(image);
   }
-  console.log("directURL REGISTER:", directUrl);
+
   const authenticateUser = (user) => {
     if (typeof user === "object") {
       setAuth((prev) => !prev);
       if (urlGiven) {
         history.push(urlGiven);
-      } else if (directUrl.includes("dashboard")) {
-        history.push(directUrl);
       } else {
         history.push("/MyPotlucks");
       }
@@ -105,90 +102,98 @@ export default function SignupForm(props) {
     <div className="authForm">
       <h1 className="pageTitle">Register</h1>
       {error && <p>{error}</p>}
-      <div className="form-container">
-        <Form className="form">
-          <div id="sections-container">
-            <section id="left-section">
-              <Form.Group controlId="formBasicFirstName">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter first name"
-                  value={firstName}
-                  onChange={(event) => setFirstName(event.target.value)}
-                />
-                <Form.Text className="text-muted"></Form.Text>
-              </Form.Group>
-              <Form.Group controlId="formBasicLastName">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter last name"
-                  value={lastName}
-                  onChange={(event) => setLastName(event.target.value)}
-                />
-                <Form.Text className="text-muted"></Form.Text>
-              </Form.Group>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Enter email"
-                  value={email}
-                  onChange={(event) => setEmail(event.target.value)}
-                />
-              </Form.Group>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Enter password"
-                  value={password}
-                  onChange={(event) => setPassword(event.target.value)}
-                />
-              </Form.Group>
-              <Form.Group controlId="formBasicPasswordConfirmation">
-                <Form.Label>Confirm Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Confirm your password"
-                  value={retypePassword}
-                  onChange={(event) => setRetypePassword(event.target.value)}
-                />
-              </Form.Group>
-            </section>
-            <section id="right-section">
-              <Form.Label>Profile Picture</Form.Label>
-              <FileBase64 multiple={true} onDone={getFile.bind(this)} />
-              {avatar.length !== 0 ? (
-                <div>
-                  {avatar.map((index, i) => {
-                    console.log(index);
-                    return (
-                      <img
-                        key={i}
-                        className="user-avatar"
-                        src={index.base64}
-                        value={avatar.name}
-                        alt="profile"
-                      />
-                    );
-                  })}
-                </div>
-              ) : null}
-            </section>
-          </div>
-          <Button variant="primary" onClick={(event) => validate(event)}>
-            Submit
-          </Button>
-        </Form>
-      </div>
-      <p className="link-reg">
-        Already have an account? &nbsp;
-        <Link className="link-reg" to="/signin">
-          Login Here
-        </Link>
-      </p>
+      <Form className="form">
+        <div id="sections-container">
+          <section class="">
+            <Form.Group controlId="formBasicFirstName">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter first name"
+                value={firstName}
+                onChange={(event) => setFirstName(event.target.value)}
+              />
+              <Form.Text className="text-muted"></Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicLastName">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter last name"
+                value={lastName}
+                onChange={(event) => setLastName(event.target.value)}
+              />
+              <Form.Text className="text-muted"></Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicPasswordConfirmation">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Confirm your password"
+                value={retypePassword}
+                onChange={(event) => setRetypePassword(event.target.value)}
+              />
+            </Form.Group>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Button variant="primary" onClick={(event) => validate(event)}>
+                Submit
+              </Button>
+              <p className="link-reg">
+                Already have an account? &nbsp;
+                <Link className="link-reg" to="/signin">
+                  Login Here
+                </Link>
+              </p>
+            </div>
+          </section>
+          <section>
+            <Form.Label>Profile Picture</Form.Label>
+            <FileBase64 multiple={true} onDone={getFile.bind(this)} />
+            {avatar.length !== 0 ? (
+              <div className="displayimage">
+                {avatar.map((index, i) => {
+                  console.log(index);
+                  return (
+                    <img
+                      key={i}
+                      className="user-avatar"
+                      src={index.base64}
+                      alt="profile"
+                    />
+                  );
+                })}
+              </div>
+            ) : null}
+          </section>
+        </div>
+      </Form>
     </div>
   );
 }
